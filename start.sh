35334c6e53c6c330f1f517ca2663fc0c17919a72
andreagiral <andreagiraldop2907@gmail.com>
2025-07-19 17:20:12 -0400

    .

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 echo "✅ Starting Rasa Action Server..."
 echo "Rasa SDK version: $(pip show rasa-sdk | grep Version)"
 echo "Python version: $(python --version)"
-rasa run actions --port 8000 --debug
+rasa run actions --port 8000 --debug 
 
 
