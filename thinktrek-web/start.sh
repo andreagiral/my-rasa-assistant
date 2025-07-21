ec6cae76d42e644a7eb338cbd375b29d5909f069
andreagiral <andreagiraldop2907@gmail.com>
2025-07-21 01:59:00 -0400

    web files

--- thinktrek-web/start.sh
+++ thinktrek-web/start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "✅ Starting Rasa Web Server..."
-rasa run \
-  --enable-api \
-  --cors "*" \
-  --debug \
-  --endpoints endpoints.yml \
-  --port 5005
+echo "Rasa version: $(rasa --version)"
+
+rasa run --enable-api \
+         --cors "*" \
+         --port 5005 \
+         --endpoints endpoints_render.yml \
+         --debug
+
+echo "❌ Rasa Web Server exited unexpectedly."
