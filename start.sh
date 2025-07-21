7cb9de0f5b2d9ef4877ff429beb8566236b077b3
andreagiral <andreagiraldop2907@gmail.com>
2025-07-21 00:09:08 -0400

    Fix

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ echo "Rasa SDK version: $(pip show rasa-sdk | grep Version)"
 echo "Python version: $(python --version)"
 
 echo "⚙️ Running Rasa SDK action server..."
-python -m rasa_sdk --port 8000 --debug
-
+rasa run actions --actions actions --port 8000 --debug
+#python -m rasa_sdk --port 8000 --debug
 echo "❌ Action Server exited unexpectedly."
 
-#rasa run actions --port 8000 --debug
+
