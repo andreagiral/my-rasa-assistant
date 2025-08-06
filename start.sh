df9e751d241cf06ad22f97786b9e77cc5b96ffe7
andreagiral <andreagiraldop2907@gmail.com>
2025-08-05 23:50:55 -0400

    CLEAN

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo "Rasa version: $(rasa --version)"
 rasa run --enable-api \
          --cors "*" \
          --port 5005 \
-         --model models/AI2.3.tar.gz \
+         --model models \
          --endpoints endpoints.yml \
          --credentials credentials.yml \
          --debug \
