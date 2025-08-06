ff61179448d79ce27b905bd595ef2a88bc0c06e3
andreagiral <andreagiraldop2907@gmail.com>
2025-08-06 05:36:50 -0400

    APP

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,6 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 import requests
-import boto3
-
-
-def download_db_from_s3():
-    s3 = boto3.client("s3")
-    try:
-        s3.download_file("thinktrek-openstax", "logs/thinktrek_logs.db", "thinktrek_logs.db")
-    except Exception as e:
-        st.error("Could not load latest log data.")
-        st.stop()
-
-download_db_from_s3()
 
 conn = sqlite3.connect("thinktrek_logs.db")
 df = pd.read_sql_query("SELECT * FROM chat_logs", conn)
